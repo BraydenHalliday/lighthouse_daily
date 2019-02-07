7dfc25f081f51c4d887cfe18191fe8e324febc73
Brayden <braydenh51@gmail.com>
2019-02-07 21:13:58 +0000

    map

---
+++ w1d4/map.js
@@ -0,0 +1,23 @@
+var words = ["ground", "control", "to", "major", "tom"];
+
+let nmap = function (words, cb) {
+ let arr = []
+  for(let i = 0; i < words.length; i++) {
+    arr.push(cb(words[i]))
+  }
+  return arr
+}
+
+
+
+nmap(words, function(word) {
+  return word.length;
+});
+
+nmap(words, function(word) {
+  return word.toUpperCase();
+});
+
+nmap(words, function(word) {
+  return word.split('').reverse().join('');
+});
